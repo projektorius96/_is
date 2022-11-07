430d3676c6f1b11cd8abaeca37ca1c9b8766bd0f
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-11-07 14:10:32 +0000

    revamped utility

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 ### NOTES
 
-> AUTHOR OF THE LIB "_isLib" : Lukas Gaucas (projektorius96)
+> Utility_name: "_is",
+> Author: Lukas Gaucas (projektorius96)
 
-> MOTIVATION : thanks to idea learn (Go)lang, & of course, thanks to the authors of Go programming language given to use programmers to explore
+--- 
 
----
-
-> INTRO : Each struct-like object destructured key property is utilised just as a placeholder for native JavaScript type itself, subsequently called on function is() this. keyword as the result implemented by functions such as checker() found in the provided examples below :..
-
-**_isLib_v1.js** is quite obselete as type (T) checking parameter must be manually typed in as follows : e.g. 'string', 'number', 'boolean'
-
-**_isLib_v2.js** (same as index.js) is improved version whereas instead of manual type (T) typed in, we just name the struct-like object destructured key property
+> INTRO : Each struct-like object destructured key property is utilised just as a placeholder for native JavaScript type itself, subsequently called on function is() KEYWORDS (THIS->SELF) e.g. THIS.number with SELF.number
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-  let {/*default:*/self, name, age, isEmployed} = {
-    self: { /* in order to destructure it to default */
-        name: (String() || "_"),
-        age: (Number() || Number(1)),
-        isEmployed: (Boolean() || !Boolean()),
-        /* ^1 : other types with placeholder values e.g. function, array, object itself */
-    },
-    name: (String() || "_"),
-    age: (Number() || Number(1)),
-    isEmployed: (Boolean() || !Boolean()),
-    /* ^1 : other types with placeholder values e.g. function, array, object itself */
-  }
+let {/*default:*/self, string, number, boolean} = {
+  self: { /* in order to destructure it to default */
+      string: "",
+      number: 123,
+      boolean: (false || true),
+      /* ^1 : other types with placeholder values e.g. function, array, object itself */
+  },
+  string: "",
+  number: 123,
+  boolean: (false || true),
+  /* ^1 : other types with placeholder values e.g. function, array, object itself */
+}
 
   let selfValueCheck = undefined ;
   
@@ -22,16 +22,16 @@
     if (false) {/* switch to else if :....... */}
     else if (typeof this.string == typeof type) {
       //console.log("correct, it's a type of: ")
-        return ["string", this.string];
+        return /* [ */"string"/* , this.string *//* ] */;
     }
     else if (typeof this.number == typeof type) {
       //console.log("correct, it's a type of: ")
-        return ["number", this.number];
+        return /* [ */"number"/* , this.number *//* ] */;
     }
     // BEWARE : in JavaScript Boolean shortcuts [ISSUE TO SOLVED]
     else if (typeof this.boolean == typeof type) {
       //console.log("correct, it's a type of: ")
-        return ["boolean", this.boolean];
+        return /* [ */"boolean"/* , this.boolean *//* ] */;
     }
     // logic flow of the remaining JavaScript types (refer to ^1 above)
     else {
@@ -41,28 +41,27 @@
 }
 
     // IMPROVEMENT : BYPASS .CALL() METHOD INVOCATION
-    function _is(/* _targetStruct, */ _type){
+    function _is(_type){
+    /* console.log("_type?", _type); */
     selfValueCheck = _type
-    return is.call(self, _type);
+    return is.call(self, _type); // DEV_NOTE: such calling inside is() function swaps e.g. this.number with self.number
     }
 
-    // EXAMPLE 2 : if passed for checker(), returned rest values of expected type T type + is(valueItself == selfValueCheck)
-    function checker(_is, T, ...rest) {
-        if (_is[0] === typeof T) {
-            console.log("matched type within resultant values (if any): ", [selfValueCheck, rest].flat())
-            return [selfValueCheck, rest].flat();
+    function checker(_is, T/*, ...rest */) {
+        /* console.log("_is[0]?", _is[0]) */
+        if (_is/*[0]*/ === typeof T) {
+            return selfValueCheck/* [selfValueCheck, rest].flat() */;
         }
         else {
             err()
         }
     }
-
-    // EXAMPLE 2 [TESTS] :
+    
     console.log("test1: ")
-    checker(_is(1), number/*, comma seperated list of values to return (if any) */)
+      console.log(checker(_is(1), number/*, comma seperated list of values to return (if any) */))
     console.log("test2: ")
-    checker(_is("1"), number/*, comma seperated list list of values to return (if any) */)
+      console.log(checker(_is("1"), number/*, comma seperated list list of values to return (if any) */))
     console.log("test3: ")
-    checker(_is(true), boolean/*, comma seperated list of values to return (if any) */)
+      console.log(checker(_is(true), boolean/*, comma seperated list of values to return (if any) */));
     console.log("test4: ")
-    checker(_is("false"), boolean/*, comma seperated list list of values to return (if any) */)
+      console.log(checker(_is("false"), boolean/*, comma seperated list list of values to return (if any) */))
