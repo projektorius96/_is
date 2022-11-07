13b992552fd919aa476315d52bb40b178c8a9234
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-11-07 14:15:27 +0000

    unwanted comment lines removed

--- index.js
+++ index.js
@@ -21,17 +21,14 @@ let {/*default:*/self, string, number, boolean} = {
   function is(type){
     if (false) {/* switch to else if :....... */}
     else if (typeof this.string == typeof type) {
-      //console.log("correct, it's a type of: ")
-        return /* [ */"string"/* , this.string *//* ] */;
+        return "string";
     }
     else if (typeof this.number == typeof type) {
-      //console.log("correct, it's a type of: ")
-        return /* [ */"number"/* , this.number *//* ] */;
+        return "number";
     }
     // BEWARE : in JavaScript Boolean shortcuts [ISSUE TO SOLVED]
     else if (typeof this.boolean == typeof type) {
-      //console.log("correct, it's a type of: ")
-        return /* [ */"boolean"/* , this.boolean *//* ] */;
+        return "boolean";
     }
     // logic flow of the remaining JavaScript types (refer to ^1 above)
     else {
@@ -41,15 +38,13 @@ let {/*default:*/self, string, number, boolean} = {
 }
 
     // IMPROVEMENT : BYPASS .CALL() METHOD INVOCATION
-    function _is(_type){
-    /* console.log("_type?", _type); */
+    function _if(_type){
     selfValueCheck = _type
     return is.call(self, _type); // DEV_NOTE: such calling inside is() function swaps e.g. this.number with self.number
     }
 
-    function checker(_is, T/*, ...rest */) {
-        /* console.log("_is[0]?", _is[0]) */
-        if (_is/*[0]*/ === typeof T) {
+    function check(_if, T/*, ...rest */) {
+        if (_if/*[0]*/ === typeof T) {
             return selfValueCheck/* [selfValueCheck, rest].flat() */;
         }
         else {
@@ -57,11 +52,12 @@ let {/*default:*/self, string, number, boolean} = {
         }
     }
     
-    console.log("test1: ")
-      console.log(checker(_is(1), number/*, comma seperated list of values to return (if any) */))
-    console.log("test2: ")
-      console.log(checker(_is("1"), number/*, comma seperated list list of values to return (if any) */))
-    console.log("test3: ")
-      console.log(checker(_is(true), boolean/*, comma seperated list of values to return (if any) */));
-    console.log("test4: ")
-      console.log(checker(_is("false"), boolean/*, comma seperated list list of values to return (if any) */))
+    // [TESTS] :
+    // console.log("test1: ")
+    //   console.log(check(_if(1), number/*, comma seperated list of values to return (if any) */))
+    // console.log("test2: ")
+    //   console.log(check(_if("1"), number/*, comma seperated list list of values to return (if any) */))
+    // console.log("test3: ")
+    //   console.log(check(_if(true), boolean/*, comma seperated list of values to return (if any) */));
+    // console.log("test4: ")
+    //   console.log(check(_if("false"), boolean/*, comma seperated list list of values to return (if any) */))
